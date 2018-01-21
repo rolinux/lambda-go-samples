1f61925057a16e0659b82e266e363c371bd8ec0f
rolinux <rolinux@users.noreply.github.com>
2018-01-21 18:38:09 +0000

    Pointing to the fork and not the original

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main_test
 import (
 	"testing"
 
-	main "github.com/aws-samples/lambda-go-samples"
+	main "github.com/rolinux/lambda-go-samples"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
